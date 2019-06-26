86b9715d415f3352e4dc231d0e4777183a586c52
Willem HOUM <212682057@ge.com>
2019-06-26 14:31:48 +0200

    Init Leaflet Map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ToastrService } from "./services/toastr.service";
+import { FooterComponent } from './components/footer/footer.component';
+import { MainMapComponent } from './components/main-map/main-map.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ToastrService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/main-map/main-map.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from "leaflet";
+@Component({
+  selector: 'app-main-map',
+  templateUrl: './main-map.component.html',
+  styleUrls: ['./main-map.component.scss']
+})
+export class MainMapComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+    const mainmap = L.map("mainmap").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mainmap);
+  }
+}
